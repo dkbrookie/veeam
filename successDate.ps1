b87b9d13b08ddf25a94eb8d15fde977771798c4e
dkbrookie <matthew@dkbinnovative.net>
2018-10-03 12:07:33 -0500

    Changed output to a string

--- successDate.ps1
+++ successDate.ps1
@@ -2,4 +2,5 @@
 Pulls the latest successful backup date for Veeam Agent
 #>
 $lastSuccess = Get-EventLog "Veeam Agent" -InstanceId 190 -Newest 1 -EntryType Information -ErrorAction SilentlyContinue
-$lastSuccess.TimeGenerated
+$lastSuccess = $lastSuccess.TimeGenerated
+"$lastSuccess"
