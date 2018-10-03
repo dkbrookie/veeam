cdb256f6c581e3e95ea362063df21e88f90940e6
dkbrookie <matthew@dkbinnovative.net>
2018-10-03 11:55:58 -0500

    Change file name

---
+++ successMessage.ps1
@@ -0,0 +1,5 @@
+<#
+Pulls the latest successful backup date for Veeam Agent
+#>
+$lastSuccess = Get-EventLog "Veeam Agent" -InstanceId 190 -Newest 1 -EntryType Information -ErrorAction SilentlyContinue
+$lastSuccess.Message
