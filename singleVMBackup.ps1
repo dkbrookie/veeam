9061d65166ff7af614ee7531bfadd9c7b2436cfa
dkbrookie <matthew@dkbinnovative.net>
2018-11-16 17:06:20 -0600

    Update singleVMBackup.ps1

--- singleVMBackup.ps1
+++ singleVMBackup.ps1
@@ -1,5 +1,6 @@
 ## Load Veeam snapin
 asnp VeeamPSSnapin -EA 0
+
 ## User Input
 $jobName = Read-Host "Enter Job Name"
 $vmName = Read-Host "Enter VM Name"
@@ -10,8 +11,7 @@ $job = Get-VBRJob | ?{$_.Name -eq $jobName}
 ## Get all objects in job apart from our target VM
 $execObjs = $job.GetObjectsInJob() | ?{$_.Name -ne $vmName}
 
-## Exclude the objects from the job(*Note: this isn't removing the objects
-## from the job)
+## Exclude the objects from the job(*Note: this isn't removing the objects from the job)
 Remove-VBRJobObject -Job $job -Objects $execObjs
 
 ## Start the job only backing upi the target VM
