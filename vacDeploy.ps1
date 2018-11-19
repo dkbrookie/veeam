87a96584bbfcc8d4afb4a497d545a9a39df09ad8
dkbrookie <matthew@dkbinnovative.net>
2018-11-19 16:42:14 -0600

    Update vacDeploy.ps1

--- vacDeploy.ps1
+++ vacDeploy.ps1
@@ -1,3 +1,8 @@
+$vccUrl = "veeam.dkbinnovative.com"
+$tenantID =
+$tenantPassword =
+
+
 #region getOSInfo
 Try {
   If((Get-WmiObject win32_operatingsystem | Select-Object -ExpandProperty osarchitecture) -eq '64-bit') {
@@ -12,22 +17,40 @@ Try {
 #endregion getOSInfo
 
 #region checkFiles
-$agentLink = "https://support.dkbinnovative.com/labtech/transfer/software/veeam/vac/vacagent$osVer.zip"
-$dlDir = "$env:windir\LTSvc\packages\veeam\VACAgent"
-$VACAgentZip = "$dlDir\VACAgent$osVer.zip"
+$vacLink = "https://support.dkbinnovative.com/labtech/transfer/software/veeam/vac/vacagent$osVer.zip"
+$vacDir = "$env:windir\LTSvc\packages\software\veeam\VACAgent"
+$VACAgentZip = "$vacDir\VACAgent$osVer.zip"
+$7zipDir = "$env:windir\LTSvc\packages\software\7zip"
+$7zipURL = "https://support.dkbinnovative.com/labtech/Transfer/software/7zip/7za.exe"
+$7zipExe = "$7zipDir\7zip.exe"
 
 Try {
-  If(!(Test-Path $dlDir)) {
-    New-Item -ItemType Directory $dlDir
+  If(!(Test-Path $vacDir)) {
+    New-Item -ItemType Directory $vacDir | Out-Null
   }
 
   If(!(Test-Path $VACAgentZip -PathType Leaf)) {
-    Start-BitsTransfer -Source $agentLink -Destination $VACAgentZip
+    Start-BitsTransfer -Source $vacLink -Destination $VACAgentZip
+    If(!(Test-Path $VACAgentZip -PathType Leaf)) {
+      Write-Error "Failed to download VACAgent zip"
+      Break
+    }
+  }
+  If(!(Test-Path $7zipDir)) {
+    New-Item -ItemType Directory $7zipDir | Out-Null
+  }
+  If(!(Test-Path $7zipExe -PathType Leaf)) {
+    Start-BitsTransfer -Source $7zipURL -Destination $7zipExe
+    If(!(Test-Path $7zipExe -PathType Leaf)) {
+      Write-Error "Failed to download VACAgent zip"
+      Break
+    }
   }
 } Catch {
   Write-Error "Failed to download the VAC Agent"
 }
 #endregion checkFiles
 
-
-msiexec /i c:\VAC.CommunicationAgent.x64.msi /qn CC_GATEWAY=veeam.dkbinnovative.com VAC_TENANT=DKB VAC_TENANT_PASSWORD=J9NL3FLEr=EgYSME
+#region installVAC
+msiexec /i "$vacDir\VAC.CommunicationAgent.x64.msi" /qn CC_GATEWAY=$vccUrl VAC_TENANT=$tenantID VAC_TENANT_PASSWORD=$tenantPassword
+#endregion installVAC
