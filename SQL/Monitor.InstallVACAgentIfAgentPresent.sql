97434195e93952f4b574ee8e3b8d6e047ea54418
dkbrookie <matthew@dkbinnovative.net>
2018-12-05 11:54:26 -0600

    New Automate monitor queries

---
+++ SQL/Monitor.InstallVACAgentIfAgentPresent.sql
@@ -0,0 +1,34 @@
+/*
+.Automate Monitor Name
+*DKBA - BACKUP - Veeam - Backup Agent Installed / Install VAC Agent
+
+.Description
+Finds all machines with Veeam Agent installed that do not have the VAC agent installed. This is used to trigger the install of the VAC
+Agent so we can control all Veeam Agents from our central management portal.
+*/
+
+SELECT DISTINCT
+  s.ComputerID AS TestValue,
+  c.Name AS IdentityField,
+  s.ComputerID,
+  acd.NoAlerts,
+  acd.UpTimeStart,
+  acd.UpTimeEnd
+FROM
+  computers c
+JOIN
+  software s
+  ON s.`ComputerID` = c.`ComputerID`
+JOIN
+  AgentComputerData acd
+  ON acd.`ComputerID` = c.`ComputerID`
+WHERE
+  s.Name = 'Veeam Agent for Microsoft Windows'
+  AND c.LastContact > DATE_ADD(NOW(),INTERVAL -15 MINUTE)
+  AND OS LIKE '%server%'
+  AND c.LocationID <> 1
+  AND s.ComputerID NOT IN (
+    SELECT computerid
+    FROM software
+    WHERE NAME = 'Veeam Availability Console Communication Agent'
+  )
