029ee4af004b41121d2df2ef5f66f72f704062c5
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-03-12 15:08:21 -0500

    Update Install.VeeamAgent.ps1
    Updated download link to Google Drive instead of the Automate server

--- Powershell/Install.VeeamAgent.ps1
+++ Powershell/Install.VeeamAgent.ps1
@@ -15,7 +15,7 @@ If($rebootKey) {
 $osVer = Get-OSBit
 
 #region checkFiles
-$agentLink = "https://support.dkbinnovative.com/labtech/transfer/software/veeam/agent/VeeamAgentWindows_2.2.0.589.exe"
+$agentLink = "https://drive.google.com/uc?export=download&id=12Cu9ZG5iEl9_Iaqod6vaj2wQj1HvS3cK"
 $agentDir = "$env:windir\LTSvc\packages\software\Veeam\Agent"
 $agentExe = "$agentDir\VeeamAgentWindows_2.2.0.589.exe"
 
