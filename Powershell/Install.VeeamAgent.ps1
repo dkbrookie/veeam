278cd8dc571c7eb890c56ff938ebc52525b899fc
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2019-03-14 13:27:37 -0500

    Update Install.VeeamAgent.ps1

--- Powershell/Install.VeeamAgent.ps1
+++ Powershell/Install.VeeamAgent.ps1
@@ -10,12 +10,8 @@ If($rebootKey) {
   Write-Host "Note there was a reboot pending, but the script auto deleted the reboot pending key to attempt to install Veeam Agent without a reboot. If the install still fails, reboot the machine before retrying."
 }
 
-## Call OS bit check script
-($WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Get-OSBit.ps1') | iex
-$osVer = Get-OSBit
-
 #region checkFiles
-$agentLink = "https://drive.google.com/uc?export=download&confirm=PwPe&id=12Cu9ZG5iEl9_Iaqod6vaj2wQj1HvS3cK"
+$agentLink = "https://support.dkbinnovative.com/labtech/transfer/software/veeam/agent/VeeamAgentWindows_2.2.0.589.exe"
 $agentDir = "$env:windir\LTSvc\packages\software\Veeam\Agent"
 $agentExe = "$agentDir\VeeamAgentWindows_2.2.0.589.exe"
 
@@ -25,8 +21,7 @@ Try {
   }
 
   If(!(Test-Path $agentExe -PathType Leaf)) {
-    ($WebClient).DownloadString('https://raw.githubusercontent.com/dkbrookie/PowershellFunctions/master/Function.Get-FileDownload.ps1') | iex
-    Get-FileDownload -FileURL $agentLink -DestinationFile $agentExe
+    (New-Object System.Net.WebClient).DownloadFile($agentLink,$agentExe)
     If(!(Test-Path $agentExe -PathType Leaf)) {
       Write-Error "Failed to download $agentExe"
       Break
